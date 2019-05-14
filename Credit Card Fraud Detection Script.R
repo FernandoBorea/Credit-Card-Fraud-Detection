4c11fec8662095aa6428c867c8c4f9bb4f9dda67
Fernando Borea <36306738+FernandoBorea@users.noreply.github.com>
2019-05-14 13:48:41 -0600

    Created Preliminary Data Exploration section

--- Credit Card Fraud Detection Script.R
+++ Credit Card Fraud Detection Script.R
@@ -13,12 +13,21 @@
 # Section 1: Data Adquisition #
 ###############################
 
-#We will first load the required libraries
+#We will first load the required libraries for the project
 
 if(!require(tidyverse)) install.packages("tidyverse", 
                                          repos = "http://cran.us.r-project.org")
 
 if(!require(RCurl)) install.packages("RCurl", 
+                                     repos = "http://cran.us.r-project.org")
+
+if(!require(knitr)) install.packages("knitr", 
+                                     repos = "http://cran.us.r-project.org")
+
+if(!require(caret)) install.packages("caret", 
+                                     repos = "http://cran.us.r-project.org")
+
+if(!require(randomForest)) install.packages("randomForest", 
                                      repos = "http://cran.us.r-project.org")
 
 #And then, if we have the file in our working directory, we will read it from there
@@ -38,3 +47,23 @@ if(file.exists("creditcard.csv"))
   cc_dataset <- read_csv(datURL)
   
 }
+
+#Once this process is finished, we can do some preliminary data exploration, for example
+
+#We can check the structure of our data with str()
+
+str(cc_dataset)
+
+#We also can create two small charts with some information about our data
+
+data.frame(Columns = ncol(cc_dataset), Rows = nrow(cc_dataset)) %>% 
+  knitr::kable()
+
+col_classes <- data.frame(Column = colnames(cc_dataset)[1:16],
+                          Class = unname(apply(cc_dataset, 2, class))[1:16],
+                          Column = c(colnames(cc_dataset)[17:ncol(cc_dataset)],""),
+                          Class = c(unname(apply(cc_dataset, 2, class))[17:ncol(cc_dataset)],""))
+
+colnames(col_classes) <- c("Column", "Class", "Column","Class")
+
+col_classes %>% knitr::kable()
