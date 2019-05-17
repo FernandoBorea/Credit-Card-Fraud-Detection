77a8949c79a9632df5b0772ba108b030bd196091
Fernando Borea <36306738+FernandoBorea@users.noreply.github.com>
2019-05-17 17:53:32 -0600

    Final model development. Conclusions and Results sections update

--- Credit Card Fraud Detection Script.R
+++ Credit Card Fraud Detection Script.R
@@ -27,7 +27,7 @@ if(!require(knitr)) install.packages("knitr",
 if(!require(caret)) install.packages("caret", 
                                      repos = "http://cran.us.r-project.org")
 
-if(!require(randomForest)) install.packages("randomForest", 
+if(!require(Rborist)) install.packages("Rborist", 
                                      repos = "http://cran.us.r-project.org")
 
 #And then, if we have the file in our working directory, we will read it from there
@@ -198,6 +198,9 @@ test_set <- cc_dataset %>% slice(-train_index)
 # Section 3: Modeling #
 #######################
 
+#Please note that  on this section we will not include a deep explaination about the code we're using as 
+#it is included on the report.
+
 #Now we will get our baseline data
 
 all_nonfraud <- rep(0, nrow(test_set)) %>% factor(levels = c("0", "1"))
@@ -252,3 +255,143 @@ colnames(glm_allvar_results) <- metrics
 
 results <- bind_rows(results, glm_allvar_results)
 results %>% knitr::kable()
+
+#Now we will train a Linear Discriminant Analysis Model with all the variables
+
+fit_lda_allvar <- train(Class ~ ., method = "lda", data = train_set)
+
+predict_lda_allvar <- predict(fit_lda_allvar, test_set)
+
+cm_lda_allvar <- confusionMatrix(data = predict_lda_allvar, reference = test_set$Class,
+                                positive = "1")
+
+#And then storing the results
+
+lda_allvar_acc <- cm_lda_allvar$overall["Accuracy"]
+lda_allvar_b_acc <- cm_lda_allvar$byClass["Balanced Accuracy"]
+lda_allvar_st <- cm_lda_allvar$byClass["Sensitivity"]
+lda_allvar_sp <- cm_lda_allvar$byClass["Specificity"]
+
+lda_allvar_results <-  data.frame(Model = "LDA - All Variables",
+                                  Accuracy = unname(lda_allvar_acc),
+                                  "Balanced Accuracy" = unname(lda_allvar_b_acc),
+                                  Sensitivity = unname(lda_allvar_st),
+                                  Specificity = unname(lda_allvar_sp), 
+                                  stringsAsFactors = FALSE)
+colnames(lda_allvar_results) <- metrics
+
+results <- bind_rows(results, lda_allvar_results)
+results %>% knitr::kable()
+
+#Now we will train a Quadratic Discriminant Analysis Model with all the variables
+
+fit_qda_allvar <- train(Class ~ ., method = "qda", data = train_set)
+
+predict_qda_allvar <- predict(fit_qda_allvar, test_set)
+
+cm_qda_allvar <- confusionMatrix(data = predict_qda_allvar, reference = test_set$Class,
+                                 positive = "1")
+
+#And then storing the results
+
+qda_allvar_acc <- cm_qda_allvar$overall["Accuracy"]
+qda_allvar_b_acc <- cm_qda_allvar$byClass["Balanced Accuracy"]
+qda_allvar_st <- cm_qda_allvar$byClass["Sensitivity"]
+qda_allvar_sp <- cm_qda_allvar$byClass["Specificity"]
+
+qda_allvar_results <-  data.frame(Model = "QDA - All Variables",
+                                  Accuracy = unname(qda_allvar_acc),
+                                  "Balanced Accuracy" = unname(qda_allvar_b_acc),
+                                  Sensitivity = unname(qda_allvar_st),
+                                  Specificity = unname(qda_allvar_sp), 
+                                  stringsAsFactors = FALSE)
+colnames(qda_allvar_results) <- metrics
+
+results <- bind_rows(results, qda_allvar_results)
+results %>% knitr::kable()
+
+#Now, as explained on the report, we will choose the QDA approach to work upon for our final model
+
+#We will start by using our naive average differences approach to select the top 5 variables to train again our model
+
+diff[1:5]
+
+fit_qda_nad5 <- train(Class ~ V3 + V14 + V17 + V12 + V10,
+                       method = "qda", data = train_set)
+
+predict_qda_nad5 <- predict(fit_qda_nad5, test_set)
+
+cm_qda_nad5 <- confusionMatrix(data = predict_qda_nad5, reference = test_set$Class,
+                                 positive = "1")
+
+#And then we will store the results
+
+qda_nad5_acc <- cm_qda_nad5$overall["Accuracy"]
+qda_nad5_b_acc <- cm_qda_nad5$byClass["Balanced Accuracy"]
+qda_nad5_st <- cm_qda_nad5$byClass["Sensitivity"]
+qda_nad5_sp <- cm_qda_nad5$byClass["Specificity"]
+
+qda_nad5_results <-  data.frame(Model = "QDA - NAD Top 5",
+                                  Accuracy = unname(qda_nad5_acc),
+                                  "Balanced Accuracy" = unname(qda_nad5_b_acc),
+                                  Sensitivity = unname(qda_nad5_st),
+                                  Specificity = unname(qda_nad5_sp), 
+                                  stringsAsFactors = FALSE)
+colnames(qda_nad5_results) <- metrics
+
+results <- bind_rows(results, qda_nad5_results)
+results %>% knitr::kable()
+
+#We will use the filterVarImp function to pick another set of predictors
+
+top_10_pred <- filterVarImp(test_set[-31], predict_qda_allvar)
+
+top_10_pred <- top_10_pred[order(top_10_pred$X0, decreasing = TRUE),]
+
+rownames(top_10_pred[1:10,])
+
+#And train again our model using those
+
+fit_qda_fvi10 <- train(Class ~ V1 + V3 + V2 + V17 + V7
+                           + V4 + V16 + V8 + V18 + V27,
+                      method = "qda", data = train_set)
+
+predict_qda_fvi10 <- predict(fit_qda_fvi10, test_set)
+
+cm_qda_fvi10 <- confusionMatrix(data = predict_qda_fvi10, reference = test_set$Class,
+                               positive = "1")
+
+#Then we will store the results
+
+qda_fvi10_acc <- cm_qda_fvi10$overall["Accuracy"]
+qda_fvi10_b_acc <- cm_qda_fvi10$byClass["Balanced Accuracy"]
+qda_fvi10_st <- cm_qda_fvi10$byClass["Sensitivity"]
+qda_fvi10_sp <- cm_qda_fvi10$byClass["Specificity"]
+
+qda_fvi10_results <-  data.frame(Model = "QDA - filterVarImp Top 10",
+                                Accuracy = unname(qda_fvi10_acc),
+                                "Balanced Accuracy" = unname(qda_fvi10_b_acc),
+                                Sensitivity = unname(qda_fvi10_st),
+                                Specificity = unname(qda_fvi10_sp), 
+                                stringsAsFactors = FALSE)
+colnames(qda_fvi10_results) <- metrics
+
+results <- bind_rows(results, qda_fvi10_results)
+results %>% knitr::kable()
+
+#We will now sort our results
+
+final_results <- results[order(results$Sensitivity, decreasing = TRUE),]
+final_results %>% knitr::kable() 
+
+#And show the confusion matrix for our final model
+
+cm_qda_allvar
+
+#We can run our final model on the entire data set
+
+qda_all_dataset <- predict(fit_qda_allvar, cc_dataset)
+
+cm_qda_all_dataset <- confusionMatrix(data = qda_all_dataset, reference = cc_dataset$Class,
+                                      positive = "1")
+cm_qda_all_dataset
